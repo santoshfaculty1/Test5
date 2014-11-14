9744ced8cd89fbb4645104005239ec5c2ef25b6d
santoshfaculty1 <nouser@noemail.com>
2014-11-14 09:24:24 +0530

    Initial Draft

---
+++ src/Test5.java
@@ -0,0 +1,15 @@
+/**
+ * Main class
+ */
+public class Test5 {
+    /**
+    * Main method
+    * @param args 
+    */
+    public static void main(String[] args) {    	
+		System.out.println("Hello world");
+    }
+    public void push(Object o){}
+    public Object pop(){}
+
+}
